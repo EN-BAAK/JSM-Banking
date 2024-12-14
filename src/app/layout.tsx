d1b290b66effe62e8ecf0af7d9484435317ba125
Bassel Ahmed ABO-Khabsa <baakswaiengineer@gmail.com>
2024-12-14 23:50:12 +0300

    create project structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
+const ibmPleSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-ple-serif"
+})
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for everyone",
+  icons: {
+    icon: "/icons/logo.svg"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibmPleSerif.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
